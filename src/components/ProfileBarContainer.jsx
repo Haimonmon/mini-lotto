43c92395d8329ecb222062cdf6e3233b41cc796b
Haimonmon <beankor8@gmail.com>
2025-03-10 16:34:32 +0800

    Added number of gamblers online

--- src/components/ProfileBarContainer.jsx
+++ src/components/ProfileBarContainer.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 
-// import "../styles/profile-bar.css";
+import QuestionableGamblersIcon from '../assets/user-question-svgrepo-com.svg';
 
 const ProfileBarContainer = () => {
     return (
-        <nav class="profile-bar-container"></nav>
+        <nav className="profile-bar-container">
+            <div className="gamblers-hub-container">
+                  <div className="online-gamblers">
+                    <div className="icon-container">
+                      <img src={QuestionableGamblersIcon} alt=""/>
+                    </div>
+                    <div className="hambler-container">
+                      <span>Gamblers</span>
+                    </div>
+                    <div className="num-online-container">
+                      <span>3</span>
+                    </div>
+                    <div className="online-icon-container">
+                      <div className="circle-lmao"></div>
+                    </div>
+                  </div>
+                </div>
+            <div className="account-container"></div>
+        </nav>
     )
 }
 
