3643097a850dc9bd4eeeb0f142865eefa6ef1ee0
Haimonmon <beankor8@gmail.com>
2025-03-12 22:38:41 +0800

    Added profile pages, not finished yet

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,7 +2,8 @@ import MainContainer from "../components/MainContainer.jsx";
 import NavigationBar from "../components/Navigation.jsx";
 import PageContainer from "../components/PageContainer.jsx";
 
-
+import GamblersProfileContainer from "../components/profile/GamblersProfileContainer.jsx";
+import CashInContainer from "../components/profile/CashInContainer.jsx";
 
 // * Not Complete 💀👌✨
 const Profile = () => {
@@ -10,7 +11,8 @@ const Profile = () => {
         <MainContainer size="large">
             <NavigationBar/>
             <PageContainer>
-                
+                <GamblersProfileContainer/>
+                <CashInContainer/>                
             </PageContainer>
         </MainContainer>
     );
