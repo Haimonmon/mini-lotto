96c5d2e33c07b9b71ed14073931b5ac45127f72f
Haimonmon <beankor8@gmail.com>
2025-03-15 07:28:28 +0800

    Merged

--- src/components/home/PlaceBetContainer.jsx
+++ src/components/home/PlaceBetContainer.jsx
@@ -4,21 +4,31 @@ const PlaceBetContainer = () => {
     return (
         <div className="place-bet-container">
             <div className="ready-bet">
-                <div className="chosen-number-container"></div>
-                <div className="place-bet-buttpn-containern">
-                    <div className="bet-card-1">
-                        <span>20</span>
+                
+                <div className="chosen-number-container">
+                    <input type="text" className="chosen-winning-num-1"/>
+                    <input type="text" className="chosen-winning-num-2"/>
+                    <input type="text" className="chosen-winning-num-3"/>
+                    <input type="text" className="chosen-winning-num-4"/>
+                    <input type="text" className="chosen-winning-num-5"/>
+                    <input type="text" className="chosen-winning-num-6"/>
+                </div>
+
+                <div className="place-bet-button-containern">
+                    <div className="bet-card-1 selected-card">
+                        <span>$10</span>
                     </div>
                     <div className="bet-card-2">
-                        <span>20</span>
+                        <span>$50</span>
                     </div>
                     <div className="bet-card-3">
-                        <span>20</span>
+                        <span>$100</span>
                     </div>
                     <div className="bet-card-4">
-                        <span>20</span>
+                        <span>$450</span>
                     </div>
                 </div>
+
             </div>
 
 
