9afafebbe49f1ac24c66c7d601fe09c2ba0e190e
Haimonmon <beankor8@gmail.com>
2025-03-15 04:56:36 +0800

    Api folder remove from ,ignore

--- src/components/home/PlaceBetContainer.jsx
+++ src/components/home/PlaceBetContainer.jsx
@@ -4,8 +4,21 @@ const PlaceBetContainer = () => {
     return (
         <div className="place-bet-container">
             <div className="ready-bet">
-                <div className="chosen-number-con"></div>
-                
+                <div className="chosen-number-container"></div>
+                <div className="place-bet-buttpn-containern">
+                    <div className="bet-card-1">
+                        <span>20</span>
+                    </div>
+                    <div className="bet-card-2">
+                        <span>20</span>
+                    </div>
+                    <div className="bet-card-3">
+                        <span>20</span>
+                    </div>
+                    <div className="bet-card-4">
+                        <span>20</span>
+                    </div>
+                </div>
             </div>
 
 
